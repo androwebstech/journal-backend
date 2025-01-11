94bda2a48dfa42a39340d47ce0930498a5721cdf
Aarti Kumari <kaarti.kc@gmail.com>
2025-01-12 01:17:53 +0530

    personal-details

--- application/models/AuthorModel.php
+++ application/models/AuthorModel.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No Access');
-class UserModel extends CI_model
+class AuthorModel  extends CI_model
 {
 	function __construct()
 	{
@@ -10,7 +10,7 @@ class UserModel extends CI_model
 	}
 public function get_personal_details($id)
 {
-    return $this->db->get_where('author', ['id' => $id])->row_array();
+    return $this->db->get_where('authors', ['id' => $id])->row_array();
 }
 
 
