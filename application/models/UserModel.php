70be346d74bd44f502f7b1d0e32a2c964569477f
Sahil <sahilluthra495@gmail.com>
2025-02-18 18:54:00 +0530

    changes

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -632,6 +632,8 @@ class UserModel extends CI_model
             research_papers.paper_title, 
             users.name,
             journals.journal_name,
+            research_papers.subjects,
+            research_papers.keywords,
             (Select name from users where users.id = publish_requests.assigned_reviewer) AS reviewer_name
         ');
         $this->db->from('publish_requests');
@@ -1095,7 +1097,7 @@ class UserModel extends CI_model
     }
     public function get_reviews_by_reviewer_id($reviewer_id)
     {
-        $this->db->select('publish_requests.reviewer_remarks , journals.journal_name ,research_papers.paper_title,research_papers.author_name,research_papers.abstract,research_papers.keywords', );
+        $this->db->select('publish_requests.reviewer_remarks , journals.journal_name ,research_papers.paper_title,research_papers.author_name,research_papers.abstract,research_papers.keywords,research_papers.subjects', );
         $this->db->from('publish_requests');
         $this->db->where('assigned_Reviewer', $reviewer_id);
         $this->db->where('pr_status!=', PR_STATUS::ACCEPT);
